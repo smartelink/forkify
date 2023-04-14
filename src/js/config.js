185dac4088b758de2d3c39c95a91a40a5b06ac41
asgers <admin@smartelink.com>
2023-04-14 15:41:31 -0400

    first commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '7106179f-0a51-4bc1-8aa0-ace607f0c046';
+export const MODAL_CLOSE_SEC = 2.5;
